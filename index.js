ef15b7a18c42cbc1205859e2cc28a2de6f02f721
Yasin <yasinabdussaburr@gmail.com>
2021-06-14 22:29:03 -0400

    commit tree 4dbdfee8321db2aeb0c4d7abcaf6807b8d18a448

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+// test 1
+let num1 = 2;
+
+let num2 = 31;
+
+let multiply = (num1*num2)
+// test 2
+let random = Math.floor(Math.random() * 100)+1;
+//test 3
+const num3 = 8;
+
+const num4 = 2;
+
+const mod = (num3 / num4);
+// test 4
+// function max() {
+//    return Math.max(1, 2, 3, 20);
+// }
+ let max = Math.max(1, 2, 3, 20);
